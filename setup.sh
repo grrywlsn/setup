ddf1983c558cadd2f638783fbaeec780f917a9df
Garry <6753180+grrywlsn@users.noreply.github.com>
2024-03-01 11:25:24 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -52,33 +52,20 @@ grep -qxF 'plugins+=(zsh-terraform)' "$HOME/.zshrc" || echo 'plugins+=(zsh-terra
 ### kubernetes
 brew install kubectl
 brew install kubectx
-brew install kubeseal
 brew install helm
 brew install stern
 
 ### aws
 brew install awscli
 brew install aws-iam-authenticator
-brew install remind101/formulae/assume-role
-brew tap wallix/awless
-brew install awless
-brew install warrensbox/tap/tfswitch
-brew install terragrunt
-brew install fluxctl
-brew install istioctl
+#brew install remind101/formulae/assume-role
+#brew tap wallix/awless
+#brew install awless
+#brew install warrensbox/tap/tfswitch
+#brew install terragrunt
+#brew install fluxctl
+#brew install istioctl
 
-### Go environment
-grep -qxF 'export GOPATH="${HOME}/.go"' "$HOME/.zshrc" || echo 'export GOPATH="${HOME}/.go"' >> "$HOME/.zshrc"
-grep -qxF 'export GOROOT="$(brew --prefix golang)/libexec"' "$HOME/.zshrc" || echo 'export GOROOT="$(brew --prefix golang)/libexec"' >> "$HOME/.zshrc"
-grep -qxF 'export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"' "$HOME/.zshrc" || echo 'export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"' >> "$HOME/.zshrc"
-source ~/.zshrc
-test -d "${GOPATH}" || mkdir "${GOPATH}"
-test -d "${GOPATH}/src/github.com" || mkdir -p "${GOPATH}/src/github.com"
-
-### Go
-brew install go
-go get golang.org/x/tools/cmd/godoc
-go get github.com/golang/lint/golint
 
 ### aliases
 grep -qxF 'alias tf="terraform"' "$HOME/.zshrc" || echo 'alias tf="terraform"' >> "$HOME/.zshrc"
